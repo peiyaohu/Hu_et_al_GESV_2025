9b95afbb2e0140a0c17569b82a98ac098b6d5371
Hu Peiyao <1241934053@qq.com>
2025-11-18 22:07:59 +0800

    Update SRR3305543.sh

--- 3_application_on_CRISPResso2Data/SRR3305543.sh
+++ 3_application_on_CRISPResso2Data/SRR3305543.sh
@@ -3,7 +3,7 @@ VSEARCH=~/biosoft/vsearch-2.30.0/bin/vsearch
     
     # Step1: Quality control
     echo "Quality filtering for $SampleID"
-    $VSEARCH --fastq_filter "../rawseq/SRR3305543.fastq" \
+    $VSEARCH --fastq_filter "./rawseq/SRR3305543.fastq" \
              --fastq_maxee 1 \
              --fastq_maxns 0 \
              --fastaout "SRR3305543.filtered.fasta" \
